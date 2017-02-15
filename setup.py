ecff2d490ea33a298a5ae42048196ce11f559035
stainbank <chad.stainbank@gmail.com>
2017-02-15 15:36:54 +0000

    Initialise simulocloud package with requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='simulocloud',
+      version='0.0',
+      description='Simulate airborne lidar scanning point clouds',
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Programming Language :: Python :: 2.7'],
+      keywords='pointcloud lidar simulation ALS TLS',
+      url='https://github.com/stainbank/simulocloud',
+      packages=find_packages()
+     ) 
+
