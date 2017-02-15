cf90f0e8a23f11098648d3090354210d396a27e6
stainbank <chad.stainbank@gmail.com>
2017-02-15 16:38:54 +0000

    Read .las to structured array in PointCloud

---
+++ simulocloud/pointcloud.py
@@ -0,0 +1,58 @@
+"""
+pointcloud
+
+Read in and store point clouds.
+"""
+
+import numpy as np
+from laspy.file import File
+
+
+class PointCloud(object):
+    """ Contains point cloud data """
+        
+    dtypes = [('x', 'f8'), ('y', 'f8'), ('z', 'f8')] # points array structure
+
+    def __init__(self, xyz):
+        """Store 3D point coordinates in a structured array.
+        
+        Arguments
+        ---------
+        xyz: sequence of len 3
+            equal sized sequences specifying 3D point coordinates (xs, ys, zs)
+        
+        Example
+        -------
+        >>> from numpy.random import rand
+        >>> n = 5 # number of points
+        >>> x, y, z = rand(n)*100, rand(n)*100., rand(n)*20
+        >>> pc = PointCloud((x,y,z))
+        >>> pc.points
+        array([( 75.37742432,  20.33372458,  14.73631503),
+               ( 39.34924712,  29.56923584,  12.15410051),
+               ( 37.11597209,  47.5210436 ,   7.08069784),
+               ( 46.30703381,  62.75060038,  17.70324372),
+               ( 25.92662908,  55.45793312,   5.95560623)], 
+              dtype=[('x', '<f8'), ('y', '<f8'), ('z', '<f8')])
+        
+        Notes
+        -----
+        The use of the constructor methods (`PointCloud.from...`) is preferred.
+        """
+        # Combine x, y and z into (flat) structured array 
+        self.points = np.column_stack(xyz).ravel().view(dtype=self.dtypes) 
+    
+    """ Constructor methods """
+    
+    @classmethod
+    def from_las(cls, fpath):
+        """Initialise PointCloud from .las file.
+
+        Arguments
+        ---------
+        fpath: str
+            filepath of .las file containing 3D point coordinates
+        
+        """ 
+        f = File(fpath)
+        return PointCloud((f.x, f.y, f.z))
