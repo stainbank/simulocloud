0ed7e87a6eeaab56d5c59a7e6874b5a5b0bab314
stainbank <chad.stainbank@gmail.com>
2017-02-16 13:26:18 +0000

    Write test data as list unless otherwise needed

--- tests/test_pointcloud.py
+++ tests/test_pointcloud.py
@@ -2,9 +2,9 @@ from simulocloud import PointCloud
 import json
 import numpy as np
 
-_TEST_XYZ = """[[10.0, 12.2, 14.4, 16.6, 18.8],
-                [11.1, 13.3, 15.5, 17.7, 19.9],
-                [0.1, 2.1, 4.5, 6.7, 8.9]]"""
+_TEST_XYZ = [[10.0, 12.2, 14.4, 16.6, 18.8],
+             [11.1, 13.3, 15.5, 17.7, 19.9],
+              [0.1, 2.1, 4.5, 6.7, 8.9]]
 
 _EXPECTED_POINTS = np.array([( 10. ,  11.1,  0.1),
                              ( 12.2,  13.3,  2.1),
@@ -15,4 +15,4 @@ _EXPECTED_POINTS = np.array([( 10. ,  11.1,  0.1),
 
 def test_PointCloud_from_lists():
     """ Can PointCloud initialisable directly from `[[xs], [ys], [zs]]` ?"""
-    assert np.all(PointCloud(json.loads(_TEST_XYZ)).points == _EXPECTED_POINTS)
+    assert np.all(PointCloud(_TEST_XYZ).points == _EXPECTED_POINTS)
