ca0b96c6f81b38e1390c5d4bcbb6e1bc5c656c1a
stainbank <chad.stainbank@gmail.com>
2017-02-16 13:35:01 +0000

    Aesthetic reformatting of tests module

--- tests/test_pointcloud.py
+++ tests/test_pointcloud.py
@@ -2,6 +2,8 @@ from simulocloud import PointCloud
 import json
 import numpy as np
 
+""" Test data """
+
 _TEST_XYZ = [[10.0, 12.2, 14.4, 16.6, 18.8],
              [11.1, 13.3, 15.5, 17.7, 19.9],
               [0.1, 2.1, 4.5, 6.7, 8.9]]
@@ -13,6 +15,9 @@ _EXPECTED_POINTS = np.array([( 10. ,  11.1,  0.1),
                              ( 18.8,  19.9,  8.9)], 
                             dtype=[('x', '<f8'), ('y', '<f8'), ('z', '<f8')])
 
-def test_PointCloud_from_lists():
-    """ Can PointCloud initialisable directly from `[[xs], [ys], [zs]]` ?"""
+""" Test functions """
+
+def test_PointCloud_read_directly_from_list():
+    """Can PointCloud initialise directly from `[[xs], [ys], [zs]]` ?"""
     assert np.all(PointCloud(_TEST_XYZ).points == _EXPECTED_POINTS)
+
