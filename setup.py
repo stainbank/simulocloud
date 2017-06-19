4ebe3783d5bb680eabff3538117bfc25fcb25fb6
stainbank <chad.stainbank@gmail.com>
2017-06-19 19:23:16 +0100

    Update version number and include matplotlib dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='simulocloud',
-      version='0.0',
+      version='0.3.2',
       description='Simulate airborne lidar scanning point clouds',
       classifiers=['Development Status :: 2 - Pre-Alpha',
                    'Programming Language :: Python :: 2.7'],
