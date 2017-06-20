47755e88368f69092db6e47b68ccf60df3e2d7e4
stainbank <chad.stainbank@gmail.com>
2017-06-20 14:27:53 +0100

    Specify dependencies, make self-installing

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='simulocloud',
-      version='0.3.2',
+      version='0.3.3',
       description='Simulate airborne lidar scanning point clouds',
       classifiers=['Development Status :: 2 - Pre-Alpha',
                    'Programming Language :: Python :: 2.7'],
       keywords='pointcloud lidar simulation ALS TLS',
       url='https://github.com/stainbank/simulocloud',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=['laspy', 'numpy', 'matplotlib']
      ) 
 
